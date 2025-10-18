1e5023c9dc574b734b4afad4597317bc770d5bb8
Chris Dodds <2152071+chrisdodds@users.noreply.github.com>
2025-10-17 20:49:52 -0600

    deploy stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://fishsticklabs.com",
   integrations: [sitemap()],
+  // Uncomment if deploying to GitHub Pages with a repo name:
+  // base: '/repo-name',
 });
 
